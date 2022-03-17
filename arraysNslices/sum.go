5fb132f2b42486457708981e5190418b96238f29
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-03-17 08:30:52 +0100

    slice with tdd

--- arraysNslices/sum.go
+++ arraysNslices/sum.go
@@ -13,3 +13,13 @@ func Sum(numbers[5]int) int {
 
 	return sum
 }
+
+	func SumUnspecified(numbers []int) interface{} {
+		sum := 0
+
+		for _, number := range numbers {
+			sum += number
+		}
+		return sum;
+	}
+
--- arraysNslices/sum_test.go
+++ arraysNslices/sum_test.go
@@ -3,11 +3,25 @@ package arraysNslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5] int{1,2,3,4,5}
 
-	got := Sum(numbers)
-	want:= 15
-	if want != got {
-		t.Errorf("Wanted %d but Got %d was Given, %v", want, got, numbers)
-	}
+	t.Run("Collection of specified numbers", func(t *testing.T) {
+		numbers := [5] int{1,2,3,4,5}
+
+		got := Sum(numbers)
+		want:= 15
+		if want != got {
+			t.Errorf("Wanted %d but Got %d was Given, %v", want, got, numbers)
+		}
+	})
+
+	t.Run("Collection for unspecified number of items", func(t *testing.T) {
+		numbers := []int{1,2,3}
+
+		got := SumUnspecified(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d but was expecting %d and was given, %v", got, want, numbers)
+		}
+	})
 }
